f357026b19804672a999b8a8cd91a32b19713222
DennySPb <dennyspb@gmail.com>
2020-10-09 09:15:42 +0300

    makefile: add prefix for pre-R policy location
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Id14b3347ca7cacf59aa92233a9ee9703d4443347

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,31 +1,31 @@
 # all socs above sdm845 or sdm710
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
+    device/qcom/sepolicy_q/generic/public \
+    device/qcom/sepolicy_q/qva/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
+    device/qcom/sepolicy_q/generic/private \
+    device/qcom/sepolicy_q/qva/private
 
 # sepolicy rules for product images
 PRODUCT_PUBLIC_SEPOLICY_DIRS := \
-     device/qcom/sepolicy/product/public
+     device/qcom/sepolicy_q/product/public
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS := \
-      device/qcom/sepolicy/product/private
+      device/qcom/sepolicy_q/product/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/generic/vendor/common \
-    device/qcom/sepolicy/qva/vendor/common/sysmonapp \
-    device/qcom/sepolicy/qva/vendor/ssg \
-    device/qcom/sepolicy/timeservice \
-    device/qcom/sepolicy/generic/vendor/timeservice \
-    device/qcom/sepolicy/qva/vendor/common \
-    device/qcom/sepolicy/generic/vendor/$(TARGET_BOARD_PLATFORM) \
-    device/qcom/sepolicy/qva/vendor/$(TARGET_BOARD_PLATFORM)
+    device/qcom/sepolicy_q/generic/vendor/common \
+    device/qcom/sepolicy_q/qva/vendor/common/sysmonapp \
+    device/qcom/sepolicy_q/qva/vendor/ssg \
+    device/qcom/sepolicy_q/timeservice \
+    device/qcom/sepolicy_q/generic/vendor/timeservice \
+    device/qcom/sepolicy_q/qva/vendor/common \
+    device/qcom/sepolicy_q/generic/vendor/$(TARGET_BOARD_PLATFORM) \
+    device/qcom/sepolicy_q/qva/vendor/$(TARGET_BOARD_PLATFORM)
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
          BOARD_SEPOLICY_DIRS += \
-              device/qcom/sepolicy/generic/vendor/test \
-              device/qcom/sepolicy/qva/vendor/test
+              device/qcom/sepolicy_q/generic/vendor/test \
+              device/qcom/sepolicy_q/qva/vendor/test
     endif
 
